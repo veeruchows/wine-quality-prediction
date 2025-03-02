60e693ad253c3b649875953beb16e512c084edab
Veeru <singuveeru101@gmail.com>
2025-03-01 19:09:20 -0500

    ingestion completed

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from datetime import datetime
+
+# Create a timestamped log filename
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Define the log directory path
+logs_path = os.path.join(os.getcwd(), "logs")
+
+# Create the logs directory if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
+
+# Define the full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == '__main__':
+    logging.info('Logging has started')
